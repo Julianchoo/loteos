dfa229f2a4a2c8e2f45704dc6f4ea1bcd9f625ee
Julianchoo <juliankorn@gmail.com>
2026-01-13 13:24:03 -0300

    fix: resolve typescript build error in Slider component

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -26,14 +26,14 @@ function Slider({
   return (
     <SliderPrimitive.Root
       data-slot="slider"
-      defaultValue={defaultValue}
-      value={value}
       min={min}
       max={max}
       className={cn(
         "relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col",
         className
       )}
+      {...(value !== undefined ? { value } : {})}
+      {...(defaultValue !== undefined ? { defaultValue } : {})}
       {...props}
     >
       <SliderPrimitive.Track
